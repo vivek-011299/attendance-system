40416f1cb144864dbb2e032838ab17270e87ec3e
vivek <vivek011299@gmail.com>
2023-11-11 15:32:22 +0530

    defined models

--- beans.go
+++ beans.go
@@ -1,1 +1,24 @@
 package main
+
+import "time"
+
+type Student struct {
+	Id    int    `json:"roll"`
+	Name  string `json:"name"`
+	Class int    `json:"class"`
+	Age   int    `json:"age"`
+	Phone string `json:"phone"`
+}
+
+type Teacher struct {
+	Id          int    `json:"id"`
+	FirstName   string `json:"firstname"`
+	LastName    string `json:"lastname"`
+	PhoneNumber string `json:"phone number"`
+}
+
+type StudentAttendance struct {
+	StudentId int `json:"roll"`
+	PunchIn  time.Time `json:"punchin"`
+	PunchOut time.Time `json:"punchout"`
+}

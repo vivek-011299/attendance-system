c53fc8b746acba911f9f3600c9edc83ac130cc4f
vivek <vivek011299@gmail.com>
2023-11-12 23:27:00 +0530

    updated object relational mapper

--- beans/beans.go
+++ beans/beans.go
@@ -3,22 +3,22 @@ package beans
 import "time"
 
 type Student struct {
-	Id    int    `json:"roll"`
-	Name  string `json:"name"`
+	Id    int    `json:"roll" gorm:"primary_key; auto_increment"`
+	Name  string `json:"name" gorm:"type:varchar(40); not null"`
 	Class int    `json:"class"`
 	Age   int    `json:"age"`
-	Phone string `json:"phone"`
+	Phone string `json:"phone" gorm:"type:varchar(40)"`
 }
 
 type Teacher struct {
-	Id          int    `json:"id"`
-	FirstName   string `json:"firstname"`
-	LastName    string `json:"lastname"`
-	PhoneNumber string `json:"phone number"`
+	Id          int    `json:"id" gorm:"primary_key; auto_increment"`
+	FirstName   string `json:"firstname" gorm:"type:varchar(40); not null"`
+	LastName    string `json:"lastname" gorm:"type:varchar(40)"`
+	PhoneNumber string `json:"phone number" gorm:"type:varchar(40)"`
 }
 
 type StudentAttendance struct {
-	StudentId int `json:"roll"`
+	StudentId int `json:"roll" gorm:"primary_key"`
 	PunchIn  time.Time `json:"punchin"`
 	PunchOut time.Time `json:"punchout"`
 }
--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-
-	"github.com/gorilla/mux"
+	"os"
 )
 
+func OpenDatabase(db *sql.DB) error{
+	db_name := os.Getenv("DB_NAME")
+	username := os.Getenv("USER")
+	driver := os.Getenv("DRIVER_NAME")
+	db, err := sql.Open(driver, "user="+username+"dbname="+db_name+"sslmode=disable")
+	fmt.Println("db is ",db)
+	if err!=nil{
+		return (err)
+	}
+	return nil
+}
+
 func main() {
-	router := mux.NewRouter()
-	
+	//router := mux.NewRouter()
+	var db *sql.DB
+	err := OpenDatabase(db)
+	if err!=nil{
+		fmt.Printf("error connecting %v",err)
+	}
 
 	fmt.Println("Starting the server at port 8000")
 }

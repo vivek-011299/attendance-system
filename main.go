9edb24e48f75b6ead400318da529a38b22c94ade
vivek <vivek011299@gmail.com>
2023-11-14 21:35:11 +0530

    db connection done

--- main.go
+++ main.go
@@ -1,30 +1,40 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"log"
+	"my-project/beans"
 	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/lib/pq"
 )
 
-func OpenDatabase(db *sql.DB) error{
-	db_name := os.Getenv("DB_NAME")
-	username := os.Getenv("USER")
-	driver := os.Getenv("DRIVER_NAME")
-	db, err := sql.Open(driver, "user="+username+"dbname="+db_name+"sslmode=disable")
-	fmt.Println("db is ",db)
-	if err!=nil{
-		return (err)
-	}
-	return nil
-}
+var db *gorm.DB
+var err error
 
 func main() {
-	//router := mux.NewRouter()
-	var db *sql.DB
-	err := OpenDatabase(db)
-	if err!=nil{
-		fmt.Printf("error connecting %v",err)
-	}
+	//loading env variables
+	dialect := os.Getenv("DRIVER")
+	//db_name := os.Getenv("DBNAME")
+	//db_port := os.Getenv("DBPORT")
+	//user := os.Getenv("USER")
+	//password := os.Getenv("PASSWORD")
+	//host:= os.Getenv("HOST")
+	dbURI := "host=localhost user=postgres dbname=attendance sslmode=disable password=postgres port=5432"
 
-	fmt.Println("Starting the server at port 8000")
+	//opening db
+	db, err = gorm.Open(dialect,dbURI)
+	if err!=nil{
+		log.Fatal(err)
+	}else{
+		fmt.Println("Starting the server at port 5432")
+	}
+	
+	defer db.Close()
+
+	db.AutoMigrate(&beans.Student{})
+	db.AutoMigrate(&beans.Teacher{})
+	db.AutoMigrate(&beans.StudentAttendance{})
 }

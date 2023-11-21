85c1b8a93b23f5c004ff527e7e46865cf885abcf
vivek <vivek011299@gmail.com>
2023-11-21 15:16:36 +0530

    db connection for student some fucntions

--- ServiceLayer/StudentService.go
+++ ServiceLayer/StudentService.go
@@ -7,12 +7,18 @@ import (
 )
 
 func StudentPunchin(student_obj beans.StudentAttendance){
-	latestDayRecord := beans.StudentAttendance{}
-	latestDayRecord = RepoLayer.Get_recent_attendance_record(student_obj.StudentId)
-	if latestDayRecord.PunchOut.IsZero(){
-		fmt.Println("You need to punchout first for the previous day.")
+	count_obj := RepoLayer.Count_of_records(student_obj.StudentId)
+	fmt.Println("count obj is :", count_obj)
+	if count_obj.PunchIn.IsZero() && count_obj.PunchOut.IsZero(){
+		RepoLayer.InsertPunchin_time(student_obj)
 	}else{
-		RepoLayer.InsertPunchin_time(student_obj.StudentId)
+		latestDayRecord := RepoLayer.Get_recent_attendance_record(student_obj.StudentId)
+		fmt.Println("Latest dy record is :",latestDayRecord)
+		if latestDayRecord.PunchOut.IsZero(){
+			fmt.Println("You need to punchout first for the previous day.")
+		}else{
+			RepoLayer.InsertPunchin_time(student_obj)
+		}
 	}
 }
 

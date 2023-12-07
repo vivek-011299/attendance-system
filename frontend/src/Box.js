dbfef5fc4c8ea84d440a6cf39baafcc5ee31bb99
vivek <vivek011299@gmail.com>
2023-12-08 00:44:20 +0530

    student page

--- frontend/src/Box.js
+++ frontend/src/Box.js
@@ -1,9 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import './Box.css';
 
 function Box(props) {
+    const navigate = useNavigate();
+    const NavigateStudent = () => {
+        if (props.text==="Student")
+            navigate("/student");
+        if(props.text==="Teacher")
+            navigate("/teacher");
+        if(props.text==="Principal")
+            navigate("/principal");
+    }
     return(
         <div className="box">
-            <button className="box-btn">{props.text}</button>
+            <button onClick={NavigateStudent} className="box-btn">{props.text}</button>
         </div>
     );
 }
--- frontend/src/student/studentPage.js
+++ frontend/src/student/studentPage.js
@@ -1,10 +1,19 @@
 import './studentPage.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Student(){
     return(
-        <div>
-                This is the student page
+        <>
+        <div className='inputbox'>
+            <h2>Enter your ID in the input box</h2>
+            <input className="searchbox" placeholder='Enter your id here'></input>
+            <button class="btn btn-info">Search</button>
         </div>
+        <div className='pipout-buttons'>
+            <button class="btn btn-primary">Punchin</button>
+            <button class="btn btn-danger">Punchout</button>
+        </div>
+        </>
     );
 }
 
